523bda6b1579aac1165e061cfa20c4748b86eb9d
Ryan Nowak <nowakra@gmail.com>
2023-12-27 15:57:41 -0800

    Add testing
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,33 +28,29 @@ export default defineConfig({
   },
 
   // Run your local dev server before starting the tests
-  webServer: process.env.CI
-    ? []
-    : [
-        {
-          command: 'yarn start',
-          port: 3000,
-          reuseExistingServer: true,
-          timeout: 60_000,
-        },
-      ],
+  webServer: {
+    command: 'yarn start',
+    port: 3000,
+    reuseExistingServer: true,
+    timeout: 60_000,
+  },
 
   forbidOnly: !!process.env.CI,
 
   retries: process.env.CI ? 2 : 0,
 
-  reporter: [['html', { open: 'never', outputFolder: 'e2e-test-report' }]],
+  reporter: [
+    ['html', { open: 'never', outputFolder: './logs/e2e-test-report' }],
+  ],
 
   use: {
     actionTimeout: 0,
-    baseURL:
-      process.env.PLAYWRIGHT_URL ??
-      (process.env.CI ? 'http://localhost:7007' : 'http://localhost:3000'),
+    baseURL: process.env.PLAYWRIGHT_URL ?? 'http://localhost:3000',
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
 
-  outputDir: 'node_modules/.cache/e2e-test-results',
+  outputDir: './logs/e2e-test-results',
 
   projects: generateProjects(), // Find all packages with e2e-test folders
 });
