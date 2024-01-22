253cbc2eab0dfc27f58f1b963334f78e7445276c
Ryan Nowak <nowakra@gmail.com>
2024-01-22 10:23:31 -0800

    Putting the dashboard on a diet (#25)
    * Remove catalog integration experiment
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>
    
    * Remove unused features
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>
    
    * Add bundle analyzer command
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>
    
    * Remove python from Dockerfile dependencies
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>
    
    ---------
    
    Signed-off-by: Ryan Nowak <nowakra@gmail.com>

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,15 +1,11 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
-import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
-import { RadiusProcessor } from '@internal/plugin-radius-backend';
 
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
-  builder.addProcessor(new ScaffolderEntitiesProcessor());
-  builder.addProcessor(new RadiusProcessor(env.reader));
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
