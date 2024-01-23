0a436393ccab555fc8c3c41cbbb2d455abcce8eb
Will Tsai <28876888+willtsai@users.noreply.github.com>
2024-01-23 14:19:50 -0800

    address link stacking feedback
    Signed-off-by: Will Tsai <28876888+willtsai@users.noreply.github.com>

--- plugins/plugin-radius/src/components/resourcebreadcrumbs/ResourceBreadcrumbs.tsx
+++ plugins/plugin-radius/src/components/resourcebreadcrumbs/ResourceBreadcrumbs.tsx
@@ -5,24 +5,14 @@ import { Breadcrumbs } from '@backstage/core-components';
 import { Typography } from '@material-ui/core';
 
 export const ResourceBreadcrumbs = (props: { resource: Resource }) => {
-  const breadcrumbs = [];
-
-  if (props.resource.properties?.environment as string) {
-    breadcrumbs.push(
-      <ResourceLink id={props.resource.properties?.environment as string} />,
-    );
-  }
-  if (props.resource.properties?.application as string) {
-    breadcrumbs.push(
-      <ResourceLink id={props.resource.properties?.application as string} />,
-    );
-  }
-
   return (
     <Breadcrumbs>
-      {breadcrumbs.map((breadcrumb, index) => (
-        <div key={index}>{breadcrumb}</div>
-      ))}
+      {props.resource.properties?.environment && (
+        <ResourceLink id={props.resource.properties?.environment as string} />
+      )}
+      {props.resource.properties?.application && (
+        <ResourceLink id={props.resource.properties?.application as string} />
+      )}
       <Typography>{props.resource.name}</Typography>
     </Breadcrumbs>
   );
