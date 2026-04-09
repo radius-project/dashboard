cc5819d917194e2800fba53acb7ff1d447c26529
Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>
2026-04-08 20:30:51 -0400

    update
    Signed-off-by: Brooke Hamilton <45323234+brooke-hamilton@users.noreply.github.com>

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -2,9 +2,6 @@
 
 set -eu
 
-# Install the current version of Radius
-# wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
-
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 # Disable corepack download prompt (silent option)
 yarn install
 yarn exec playwright install chrome
